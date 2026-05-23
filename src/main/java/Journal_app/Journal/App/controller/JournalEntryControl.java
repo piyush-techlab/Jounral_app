4101b8ba452bed907d5e9480f7236154b007e5c7
Peeyush Mohanty <peeyushmoha.nty222@gmail.com>
2026-05-24 00:40:24 +0530

    Added Delete, Update APIs

--- src/main/java/Journal_app/Journal/App/controller/JournalEntryControl.java
+++ src/main/java/Journal_app/Journal/App/controller/JournalEntryControl.java
@@ -11,8 +11,6 @@ import java.util.*;
 @RequestMapping("/Journal")
 public class JournalEntryControl {
 
-
-
     Map<Integer , JournalEntry> journalEntries = new HashMap();
 
     @GetMapping
@@ -21,9 +19,28 @@ public class JournalEntryControl {
         return new ArrayList<>(journalEntries.values());
     }
 
-
     @PostMapping
     public boolean setValues(@RequestBody JournalEntry journalentry)
+    {
+        journalEntries.put(journalentry.getId(), journalentry);
+        return true;
+    }
+
+    @GetMapping ("id/{map_id}")
+    public JournalEntry getDetails_byID(@PathVariable int map_id)
+    {
+        return journalEntries.get(map_id);
+    }
+
+    @DeleteMapping ("id/{map_id}")
+    public boolean deletedeatilsby_id(@PathVariable int map_id)
+    {
+        journalEntries.remove(map_id);
+        return true;
+    }
+
+    @PutMapping ("id/{map_id}")
+    public boolean updatedeatilsby_id(@PathVariable int map_id, @RequestBody JournalEntry journalentry)
     {
         journalEntries.put(journalentry.getId(), journalentry);
         return true;
